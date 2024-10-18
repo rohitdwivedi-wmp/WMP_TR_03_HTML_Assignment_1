628d31eb9d8335f3eafcc63082f180fe4d5cf3ef
Rohit Kumar Dwivedi <rohitdwivedi@wingmanpartners.com>
2024-10-18 14:56:34 +0530

    HTML Assignment 1

---
+++ app.js
@@ -0,0 +1,10 @@
+let menuBtn = document.querySelector('#menu-btn');
+let navBtns = document.getElementById('mobile-view-nav');
+
+menuBtn.addEventListener('click',function(){
+    if(menuBtn.classList.contains('fa-bars')){
+        menuBtn.classList.replace('fa-bars','fa-times');
+    }
+    else menuBtn.classList.replace('fa-times','fa-bars');
+    navBtns.classList.toggle('menu-show');
+});
